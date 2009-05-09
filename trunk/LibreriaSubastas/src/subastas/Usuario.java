5ffe436e77b236240d02604f64c167ee1691ef1a
louisantoniogarcia@gmail.com <louisantoniogarcia@gmail.com@e0da2c0c-33f3-11de-8fef-1f5ae417a2df>
2009-05-09 18:49:40 +0000

    asdfasdf

---
+++ trunk/LibreriaSubastas/src/subastas/Usuario.java
@@ -0,0 +1,150 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package subastas;
+
+/**
+ *
+ * @author Louis
+ */
+public class Usuario
+{
+    private String usuario;
+    private String clave;
+    private String email;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String ciudad;
+    private String pais;
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario()
+    {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario)
+    {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the clave
+     */
+    public String getClave()
+    {
+        return clave;
+    }
+
+    /**
+     * @param clave the clave to set
+     */
+    public void setClave(String clave)
+    {
+        this.clave = clave;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail()
+    {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos()
+    {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos)
+    {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion()
+    {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion)
+    {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the ciudad
+     */
+    public String getCiudad()
+    {
+        return ciudad;
+    }
+
+    /**
+     * @param ciudad the ciudad to set
+     */
+    public void setCiudad(String ciudad)
+    {
+        this.ciudad = ciudad;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais()
+    {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais)
+    {
+        this.pais = pais;
+    }
+}
