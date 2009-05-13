0784334351f04b6149c1629e917d8e3bdbba37ef
louisantoniogarcia@gmail.com <louisantoniogarcia@gmail.com@e0da2c0c-33f3-11de-8fef-1f5ae417a2df>
2009-05-13 09:07:54 +0000

    Clases para contener los datos creadas

--- trunk/ServidorSubastas/src/java/dao/SubastasDAO.java
+++ trunk/ServidorSubastas/src/java/dao/SubastasDAO.java
@@ -6,6 +6,7 @@
 package dao;
 
 import java.sql.*;
+import java.util.ArrayList;
 import subastas.Subasta;
 import subastas.Usuario;
 
@@ -83,65 +84,267 @@ public class SubastasDAO
 
             ps.close();
         } catch (SQLException ex) { }
+
         return usu;
     }
 
 	public Subasta[] obtenerTodasLasSubastas()
 	{
-		return null;
+		ArrayList<Subasta> subs = new ArrayList<Subasta>();
+		try {
+            String consulta = "SELECT * FROM DAD.SUBASTAS";
+
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+				Subasta sub = new Subasta();
+				sub.setId(rs.getInt("id"));
+				sub.setNombre(rs.getString("nombre"));
+				sub.setSubastador(obtenerUsuario(rs.getString("subastador")));
+				sub.setDescripcion(rs.getString("descripcion"));
+				sub.setCategoria(rs.getString("categoria"));
+				sub.setPrecioSalida(rs.getDouble("precio_salida"));
+				sub.setPujaActual(rs.getDouble("puja_actual"));
+				sub.setPrecioCompra(rs.getDouble("precio_compra"));
+
+				sub.setFechaSalida(new java.util.Date(rs.getDate("fecha_salida").getTime()));
+				sub.setFechaCierre(new java.util.Date(rs.getDate("fecha_cierre").getTime()));
+
+				Date d = rs.getDate("fecha_puja_actual");
+				if(d != null)
+					sub.setFechaPujaActual(new java.util.Date(d.getTime()));
+
+				sub.setPujadorActual(obtenerUsuario(rs.getString("pujador_actual")));
+
+				subs.add(sub);
+            }
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+		return (Subasta[])subs.toArray();
+	}
+
+	public Subasta[] obtenerTodasLasSubastasAbiertas()
+	{
+		ArrayList<Subasta> subs = new ArrayList<Subasta>();
+		try {
+            String consulta = "SELECT * FROM DAD.SUBASTAS WHERE fecha_cierre > CURRENT_TIMESTAMP";
+
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+				Subasta sub = new Subasta();
+				sub.setId(rs.getInt("id"));
+				sub.setNombre(rs.getString("nombre"));
+				sub.setSubastador(obtenerUsuario(rs.getString("subastador")));
+				sub.setDescripcion(rs.getString("descripcion"));
+				sub.setCategoria(rs.getString("categoria"));
+				sub.setPrecioSalida(rs.getDouble("precio_salida"));
+				sub.setPujaActual(rs.getDouble("puja_actual"));
+				sub.setPrecioCompra(rs.getDouble("precio_compra"));
+
+				sub.setFechaSalida(new java.util.Date(rs.getDate("fecha_salida").getTime()));
+				sub.setFechaCierre(new java.util.Date(rs.getDate("fecha_cierre").getTime()));
+
+				Date d = rs.getDate("fecha_puja_actual");
+				if(d != null)
+					sub.setFechaPujaActual(new java.util.Date(d.getTime()));
+
+				sub.setPujadorActual(obtenerUsuario(rs.getString("pujador_actual")));
+
+				subs.add(sub);
+            }
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+		return (Subasta[])subs.toArray();
 	}
 
 	public Subasta[] obtenerSubastasDeUsuario(Usuario u)
 	{
-		return null;
+		ArrayList<Subasta> subs = new ArrayList<Subasta>();
+		try {
+            String consulta = "SELECT * FROM DAD.SUBASTAS WHERE subastador='"+u.getUsuario()+"'";
+
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+				Subasta sub = new Subasta();
+				sub.setId(rs.getInt("id"));
+				sub.setNombre(rs.getString("nombre"));
+				sub.setSubastador(obtenerUsuario(rs.getString("subastador")));
+				sub.setDescripcion(rs.getString("descripcion"));
+				sub.setCategoria(rs.getString("categoria"));
+				sub.setPrecioSalida(rs.getDouble("precio_salida"));
+				sub.setPujaActual(rs.getDouble("puja_actual"));
+				sub.setPrecioCompra(rs.getDouble("precio_compra"));
+
+				sub.setFechaSalida(new java.util.Date(rs.getDate("fecha_salida").getTime()));
+				sub.setFechaCierre(new java.util.Date(rs.getDate("fecha_cierre").getTime()));
+
+				Date d = rs.getDate("fecha_puja_actual");
+				if(d != null)
+					sub.setFechaPujaActual(new java.util.Date(d.getTime()));
+
+				sub.setPujadorActual(obtenerUsuario(rs.getString("pujador_actual")));
+
+				subs.add(sub);
+            }
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+		return (Subasta[])subs.toArray();
 	}
 
-	public Subasta obtenerSubasta(Object parametro)
+	public Subasta obtenerSubasta(int id)
 	{
-		return null;
+		Subasta sub = null;
+		try {
+            String consulta = "SELECT * FROM DAD.SUBASTAS WHERE ID="+id;
+
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+				sub = new Subasta();
+				sub.setId(id);
+				sub.setNombre(rs.getString("nombre"));
+				sub.setSubastador(obtenerUsuario(rs.getString("subastador")));
+				sub.setDescripcion(rs.getString("descripcion"));
+				sub.setCategoria(rs.getString("categoria"));
+				sub.setPrecioSalida(rs.getDouble("precio_salida"));
+				sub.setPujaActual(rs.getDouble("puja_actual"));
+				sub.setPrecioCompra(rs.getDouble("precio_compra"));
+
+				sub.setFechaSalida(new java.util.Date(rs.getDate("fecha_salida").getTime()));
+				sub.setFechaCierre(new java.util.Date(rs.getDate("fecha_cierre").getTime()));
+
+				Date d = rs.getDate("fecha_puja_actual");
+				if(d != null)
+					sub.setFechaPujaActual(new java.util.Date(d.getTime()));
+
+				sub.setPujadorActual(obtenerUsuario(rs.getString("pujador_actual")));
+            }
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+		return sub;
 	}
 
 	public boolean insertarSubasta(Subasta s)
 	{
-		return false;
+		boolean resultado = false;
+		try {
+			String columnas = "INSERT INTO DAD.SUBASTAS (NOMBRE,SUBASTADOR,DESCRIPCION,CATEGORIA,PRECIO_SALIDA,PUJA_ACTUAL,PRECIO_COMPRA,FECHA_SALIDA,FECHA_CIERRE,FECHA_PUJA_ACTUAL,PUJADOR_ACTUAL) VALUES ";
+			StringBuffer values = new StringBuffer("(");
+
+			values.append("'"+s.getNombre()+"'");
+			values.append(",'"+s.getSubastador().getUsuario()+"'");
+			if(s.getDescripcion() != null)
+				values.append(",'"+s.getDescripcion()+"'");
+			else
+				values.append(",NULL");
+			if(s.getCategoria() != null)
+				values.append(",'"+s.getCategoria()+"'");
+			else
+				values.append(",DEFAULT");
+			values.append(","+s.getPrecioSalida());
+			values.append(","+s.getPujaActual());
+			values.append(","+s.getPrecioCompra());
+
+			Timestamp ts = new Timestamp(s.getFechaSalida().getTime());
+			values.append(",'"+ts+"'");
+			ts.setTime(s.getFechaCierre().getTime());
+			values.append(",'"+ts+"'");
+			if (s.getFechaPujaActual() != null) {
+				ts.setTime(s.getFechaPujaActual().getTime());
+				values.append(",'"+ts+"'");
+			} else
+				values.append(",NULL");
+			if (s.getPujadorActual() != null)
+				values.append(",'"+s.getPujadorActual().getUsuario()+"'");
+			else
+				values.append(",NULL");
+
+			values.append(")");
+
+            PreparedStatement ps = conexion.prepareStatement(columnas+values.toString());
+            int f = ps.executeUpdate();
+            resultado = f > 0;
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+        return resultado;
 	}
 
 	public boolean borrarSubasta(Subasta s)
 	{
-		return false;
+		boolean resultado = false;
+		try {
+			String consulta = "DELETE FROM DAD.SUBASTAS WHERE ID="+s.getId();
+
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+            int f = ps.executeUpdate();
+            resultado = f > 0;
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+        return resultado;
 	}
 
 	public boolean actualizarSubasta(Subasta s)
 	{
-		return false;
+		boolean resultado = false;
+		try {
+			String columnas = "UPDATE DAD.SUBASTAS SET";
+			StringBuffer values = new StringBuffer();
+
+			values.append("NOMBRE='"+s.getNombre()+"'");
+			values.append(",SUBASTADOR='"+s.getSubastador().getUsuario()+"'");
+			if(s.getDescripcion() != null)
+				values.append(",DESCRIPCION='"+s.getDescripcion()+"'");
+			else
+				values.append(",DESCRIPCION=NULL");
+			if(s.getCategoria() != null)
+				values.append(",CATEGORIA='"+s.getCategoria()+"'");
+			else
+				values.append(",CATEGORIA=DEFAULT");
+			values.append(",PRECIO_SALIDA="+s.getPrecioSalida());
+			values.append(",PUJA_ACTUAL="+s.getPujaActual());
+			values.append(",PRECIO_COMPRA="+s.getPrecioCompra());
+
+			Timestamp ts = new Timestamp(s.getFechaSalida().getTime());
+			values.append(",FECHA_SALIDA='"+ts+"'");
+			ts.setTime(s.getFechaCierre().getTime());
+			values.append(",FECHA_CIERRE='"+ts+"'");
+			if (s.getFechaPujaActual() != null) {
+				ts.setTime(s.getFechaPujaActual().getTime());
+				values.append(",FECHA_PUJA_ACTUAL='"+ts+"'");
+			} else
+				values.append(",FECHA_PUJA_ACTUAL=NULL");
+			if (s.getPujadorActual() != null)
+				values.append(",PUJADOR_ACTUAL='"+s.getPujadorActual().getUsuario()+"'");
+			else
+				values.append(",PUJADOR_ACTUAL=NULL");
+
+			values.append(" WHERE ID="+s.getId());
+
+            PreparedStatement ps = conexion.prepareStatement(columnas+values.toString());
+            int f = ps.executeUpdate();
+            resultado = f > 0;
+
+            ps.close();
+        } catch (SQLException ex) { }
+
+        return resultado;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
